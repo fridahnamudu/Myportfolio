cab408b75b2de3a9e16980f6cd8604bfe243c3fc
fridahnamudu <fridah.namudu@gmail.com>
2024-02-24 13:02:31 +0300

    add changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './intro.css'
-import bg from '../../assets/newnamudu2.png'
+import bg from '../../assets/namudu.png'
 import {Link} from 'react-scroll'
 import WorkIcon from '@mui/icons-material/Work';
 import styled from '@emotion/styled';
